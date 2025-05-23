8b1d9d8b54f7e7e89d4b20f593acf1e82b7f01e9
loAnto98 <antoniol982016@gmail.com>
2025-05-23 16:46:37 +0200

    Update itaevents.py

--- itaevents.py
+++ itaevents.py
@@ -13,8 +13,8 @@ import re
 from urllib.parse import quote_plus  # Add this import
 
 
-MFPLINK = ""     # non mettere lo / finale al link
-MFPPSW = ""
+MFPLINK = "https://nerek-3k5d.onrender.com"     # non mettere lo / finale al link
+MFPPSW = "2908199826Anni"
 
 
 # Constants
