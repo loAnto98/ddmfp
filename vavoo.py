9b5e9941eadb73e78c878f168f2ad1c34c74d456
loAnto98 <antoniol982016@gmail.com>
2025-05-23 16:48:16 +0200

    Update vavoo.py

--- vavoo.py
+++ vavoo.py
@@ -3,10 +3,10 @@ import json
 import re
 import os
 
-USREPG = ""
-BRANCHEPG = ""
-MFPLINK = ""     # non mettere lo / finale al link
-MFPPSW = ""
+USREPG = "loAnto98"
+BRANCHEPG = "ddmfp"
+MFPLINK = "https://nerek-3k5d.onrender.com"     # non mettere lo / finale al link
+MFPPSW = "2908199826Anni"
 
 PROXY = f"{MFPLINK}/proxy/hls/manifest.m3u8?api_password={MFPPSW}&d="
 HEADER = "&h_user-agent=VAVOO/2.6&h_referer=https://vavoo.to/"
